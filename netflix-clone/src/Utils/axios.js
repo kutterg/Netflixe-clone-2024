a67e6ff7fee32f4e9f0d0c36321302b53342ec5c
kutterg <butterg071@gmail.com>
2024-09-04 15:07:07 -0500

    axios and requests

---
+++ netflix-clone/src/Utils/axios.js
@@ -0,0 +1,12 @@
+//axios use to feach data
+import axios from "axios";
+I
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",//base URL start for all movie acess
+});
+export default instance;
+
+
+//for movie//https://api.themoviedb.org/3/discover/tv?api_key=9a23e9a8212b4d3000a10c25901b4430&with_networks=213
+
+//for img//https://image.tmdb.org/1/p/originall
